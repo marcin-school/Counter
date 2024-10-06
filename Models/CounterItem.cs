7a6aecffe94835758ed1b650f3f4613b068cd204
Marcin <marcin.morawiec8@gmail.com>
2024-10-06 19:07:37 +0200

    Create CounterItem Model

---
+++ Models/CounterItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Counter.Models
+{
+    public class CounterItem
+    {
+        public int Id { get; set; }
+        public String Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public int InitialValue { get; set; }
+        public int Value { get; set; }
+        public CounterItem() { }
+        public CounterItem(String Name, String Description, int InitialValue) {
+            this.Name = Name;
+            this.Description = Description;
+            this.InitialValue = InitialValue;
+        }
+    }
+}
