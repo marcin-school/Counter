e651af560a232cb9b07c634e84158f7e432dbbaf
Marcin <marcin.morawiec8@gmail.com>
2024-10-13 01:29:08 +0200

    Add editing and removing counters from the ListView

--- Models/AllCounters.cs
+++ Models/AllCounters.cs
@@ -1,11 +1,36 @@
-﻿namespace Counter.Models
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Counter.Models
 {
-    public class AllCounters
+    public class AllCounters : INotifyPropertyChanged
     {
+        public ICommand RemoveItemCommand { get; }
+        public ICommand EditItemCommand { get; }
+        public AllCounters() {
+            RemoveItemCommand = new Command<CounterItem>(RemoveItem);
+            EditItemCommand = new Command<CounterItem>(EditItem);
+        }
+
         public List<CounterItem> Items {
             get {
                 return App.Database.GetItems();
             }
+        }
+
+        private void RemoveItem(CounterItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EditItem(CounterItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
